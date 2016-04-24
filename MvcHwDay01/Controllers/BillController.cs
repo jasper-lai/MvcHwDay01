71b56218b6a314378f12460683e5160aafe80658
jasper-lai <jasper.lai@gmail.com>
2016-04-24 16:49:21 +0800

    練習 Delete 的功能, 但發現有一些問題, 想請老師幫忙看一下有沒有比較好的處理方式, 或者實務上通常會如何處理?
    (1) BillController.cs 裡的 DeleteConfirmed() 方法
    (2) BillingService.cs 裡的 Delete() 方法

--- MvcHwDay01/Controllers/BillController.cs
+++ MvcHwDay01/Controllers/BillController.cs
@@ -25,6 +25,10 @@ namespace MvcHwDay01.Controllers
             return View();
         }
 
+        /// <summary>
+        /// 建立資料 (HttpGet)
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
         public ActionResult Create()
         {
@@ -36,6 +40,11 @@ namespace MvcHwDay01.Controllers
             return View(obj);
         }
 
+        /// <summary>
+        /// 建立資料 (HttpPost)
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
         [HttpPost]
         public ActionResult Create(BillingItemViewModel item)
         {
@@ -54,6 +63,10 @@ namespace MvcHwDay01.Controllers
             return View();
         }
 
+        /// <summary>
+        /// 列出資料 (ChildActionOnly)
+        /// </summary>
+        /// <returns></returns>
         [ChildActionOnly]
         public ActionResult ListCurrent()
         {
@@ -63,6 +76,68 @@ namespace MvcHwDay01.Controllers
 
             return View(bills);
         }
+
+        #region 練習 Delete 的功能
+
+        /// <summary>
+        /// 刪除資料 (HttpGet)
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public ActionResult Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            //這裡必須用 id.Value, 不然編譯會出錯
+            BillingItemViewModel item = _billingSvc.GetSingle(id.Value);
+            if (item == null)
+            {
+                return HttpNotFound();
+            }
+            item.BillTypeName = GlobalCodeMappings.BillTypes[item.BillType];
+            return View(item);
+        }
+
+
+        /// <summary>
+        /// 刪除資料 (HttpPost)
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        /// <remarks>
+        /// 問題: Delete 似乎只會 POST id 欄位, 而不是整個 ViewModel 物件
+        /// 原因推測: Delete 的 HttpGet 頁面, 純粹顯示, 沒有 <input .../>; 所以不會回傳
+        /// </remarks>
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult DeleteConfirmed(Guid id)
+        {
+            //方式一: 傳整個物件到 Service 層, 但這樣需要 3 次資料庫存取
+            //(1) _billingSvc.GetSingle(id)
+            //(2) in Service: var acc2 = _db.AccountBooks.Find(item.Id);
+            //(3) _billingSvc.Save()
+            var item = _billingSvc.GetSingle(id);
+            _billingSvc.Delete(item);
+
+            ////方式二: 只需傳 Id 到 Service 層, 但這樣需要 2 次的資料庫存取
+            ////(1) in Service: var acc = _db.AccountBooks.Find(id);
+            ////(2) _billingSvc.Save()
+            //_billingSvc.DeleteById(id);
+
+            //------------------------------------------------------------
+            //[問題] 有沒有辦法直接刪掉? 只要一次的資料庫存取, 類似 DELETE ... FROM ... WHERE ...
+            //------------------------------------------------------------
+
+            _billingSvc.Save();
+            return RedirectToAction("Create");
+        }
+
+        #endregion
+
+
 
     }
 }
--- MvcHwDay01/Models/Services/BillingService.cs
+++ MvcHwDay01/Models/Services/BillingService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
@@ -45,6 +46,7 @@ namespace MvcHwDay01.Models.Services
                         .Select(x =>
                            new BillingItemViewModel
                            {
+                               Id = x.Id,
                                BillType = x.Categoryyy,
                                BillDate = x.Dateee,
                                Amount = x.Amounttt,
@@ -98,6 +100,7 @@ namespace MvcHwDay01.Models.Services
                         .Select(x =>
                            new BillingItemViewModel
                            {
+                               Id = x.Id,
                                BillType = x.Categoryyy,
                                BillDate = x.Dateee,
                                Amount = x.Amounttt,
@@ -121,6 +124,7 @@ namespace MvcHwDay01.Models.Services
                         .Select(x =>
                            new BillingItemViewModel
                            {
+                               Id = x.Id,
                                BillType = x.Categoryyy,
                                BillDate = x.Dateee,
                                Amount = x.Amounttt,
@@ -133,6 +137,76 @@ namespace MvcHwDay01.Models.Services
         #endregion
 
 
+        #region 練習 Delete 的功能
+
+        public void Delete(BillingItemViewModel item)
+        {
+            var acc = new AccountBook
+            {
+                Id = item.Id,
+                Categoryyy = item.BillType,
+                Dateee = item.BillDate,
+                Amounttt = item.Amount,
+                Remarkkk = item.Memo
+            };
+
+            ////狀況一: 
+            ////錯誤訊息: The object cannot be deleted because it was not found in the ObjectStateManager 
+            //_db.AccountBooks.Remove(acc);
+
+            ////狀況二:
+            ////作法參考: http://stackoverflow.com/questions/15945172/the-object-cannot-be-deleted-because-it-was-not-found-in-the-objectstatemanager
+            ////例外發生點: _db.AccountBooks.Attach(acc);
+            ////錯誤訊息: {"附加類型 'MvcHwDay01.Models.AccountBook' 的實體失敗，因為另一個相同類型的實體已經有相同的主索引鍵值。如果圖形中的任何實體有衝突的索引值鍵，在使用 'Attach' 方法或將實體的狀態設為 'Unchanged' 或 'Modified' 時，就可能發生這種狀況。
+            ////          原因可能是有些實體是新增的，尚未收到資料庫產生的索引鍵值。如有這種狀況，請使用 'Add' 方法或 'Added' 實體狀態來追蹤圖形，然後再視需要將非新增實體的狀態設為 'Unchanged' 或 'Modified'。"}
+            ////
+            ////原因推測: 在 Controller 的 DeleteConfirm() 裡, 有呼叫 _billingSvc.GetSingle(), 在整個 dbcontext 裡, 已存在一筆資料...
+            //var entry = _db.Entry<AccountBook>(acc);
+            //if (entry.State == EntityState.Detached)
+            //{
+            //    _db.AccountBooks.Attach(acc);
+            //}
+            //_db.AccountBooks.Remove(acc);
+
+            ////狀況三: 查了 stack overflow
+            ////作法參考: http://stackoverflow.com/questions/15637965/the-object-cannot-be-deleted-because-it-was-not-found-in-the-objectstatemanager
+            ////作法原理: 強迫 Entity Framework 不要檢查 ... 副作用?
+            ////例外發生點: _db.AccountBooks.Attach(acc);
+            ////錯誤訊息: {"附加類型 'MvcHwDay01.Models.AccountBook' 的實體失敗，因為另一個相同類型的實體已經有相同的主索引鍵值。如果圖形中的任何實體有衝突的索引值鍵，在使用 'Attach' 方法或將實體的狀態設為 'Unchanged' 或 'Modified' 時，就可能發生這種狀況。
+            ////          原因可能是有些實體是新增的，尚未收到資料庫產生的索引鍵值。如有這種狀況，請使用 'Add' 方法或 'Added' 實體狀態來追蹤圖形，然後再視需要將非新增實體的狀態設為 'Unchanged' 或 'Modified'。"}
+            //bool oldValidateOnSaveEnabled = _db.Configuration.ValidateOnSaveEnabled;
+            //try
+            //{
+            //    _db.Configuration.ValidateOnSaveEnabled = false;
+            //    var entry = _db.Entry<AccountBook>(acc);
+            //    if (entry.State == EntityState.Detached)
+            //    {
+            //        _db.AccountBooks.Attach(acc);
+            //    }
+            //    _db.Entry(acc).State = EntityState.Deleted;
+            //}
+            //finally
+            //{
+            //    _db.Configuration.ValidateOnSaveEnabled = oldValidateOnSaveEnabled;
+            //}
+
+            //狀況四: 看來是可以運作的方式
+            //作法原理: 重新由 DB 讀一次後, 再作 Remove
+            //副作用: 由 DB 再讀取一次
+            var acc2 = _db.AccountBooks.Find(item.Id);
+            _db.AccountBooks.Remove(acc2);
+
+            ////[問題]: 怎麼作才是比較好的方式, 可以避開再由資料庫讀一次的問題?
+        }
+
+
+        public void DeleteById(Guid id)
+        {
+            var acc = _db.AccountBooks.Find(id);
+            _db.AccountBooks.Remove(acc);
+        }
+
+        #endregion
 
     }
 }
