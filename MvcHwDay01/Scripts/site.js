2b7c3fea5d3e8b1c6becfe265414b0026e990d4c
jasper-lai <jasper.lai@gmail.com>
2016-05-11 15:46:30 +0800

    加入 DateOnly 的 editor template, 但有個問題: 曾試著將綁定 datetimepicker 的程式段, 移到一個獨立的 site.js, 並在 _Layout.cshtml 引入;  但發生在點取日期欄位後, 無法展開的問題 (in  IE11 + Windows 7), 尚未在其它平台的 IE 作測試) , 但 Chrome / Firefox 不會有該狀況.

--- MvcHwDay01/Scripts/site.js
+++ MvcHwDay01/Scripts/site.js
@@ -5,3 +5,15 @@ function ajaxFailedFunc(ajaxContext) {
     });
 }
 
+////[問題]: 放在這裡, 且在 _Layout.cshtml 的最末加入 script src; IE11 (in Windows 7) 無法展開日期選單.
+////註: 在 Chrome / Firefox is OK
+////註: 若放在 _Layout.cshtml, 則 Chrome / Firefox / IE11 都 OK
+//$(function () {
+//    $("[data-datetimepicker]").datetimepicker({
+//        closeOnDateSelect: true,
+//        format: 'Y-m-d',
+//        timepicker: false,
+//        //lang: 'zh-TW',
+//        //formatDate: 'Y-m-d H:i',
+//    });
+//});
